b4ad164c1a56836ae4e30f70098258172abf2d94
Mohamed <mohamedadham27@gmail.com>
2019-07-11 03:30:16 +0200

    Updates

---
+++ precache-manifest.4f2ff35cdf6f205df53f35d46929f19d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d479c88c6b01c5c596cbf3ec1bfdc101",
+    "url": "/random-Quotes-using-react.git/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/random-Quotes-using-react.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "914977b4c6f4f304066c",
+    "url": "/random-Quotes-using-react.git/static/js/main.747b7a8e.chunk.js"
+  },
+  {
+    "revision": "96b3a14daf1e33ae498c",
+    "url": "/random-Quotes-using-react.git/static/js/runtime~main.9ddced3a.js"
+  }
+]);
